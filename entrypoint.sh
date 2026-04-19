7d4825b27d963d8ca667a1fab98a2b16cb577445
Dany Setiyawan <danysetiyawan50@gmail.com>
2026-04-19 09:45:16 +0700

    fix: add astrahost-banner call to nodejs entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,14 @@ cd /home/container || exit 1
 TZ=${TZ:-UTC}
 export TZ
 
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+INTERNAL_IP=$(ip route get 1 2>/dev/null | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-printf "\033[1m\033[33m$(whoami)@$(hostname)~ \033[0m%s\n" "$STARTUP"
+if [ -x /usr/local/bin/astrahost-banner ]; then
+    /usr/local/bin/astrahost-banner
+fi
+
+STARTUP=${STARTUP:-npm start}
+
+printf "\033[1m\033[33m%s@astrahost~ \033[0m%s\n" "$(whoami)" "$STARTUP"
 eval ${STARTUP}
